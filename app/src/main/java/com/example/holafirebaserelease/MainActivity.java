59ee709f5dbd4a7d45bce43b7c09e26250f2be82
anggonzales <anggonzalesca@upt.pe>
2020-02-11 18:50:59 -0500

    Butternife y comunicacion consola

--- app/src/main/java/com/example/holafirebaserelease/MainActivity.java
+++ app/src/main/java/com/example/holafirebaserelease/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.holafirebaserelease;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -13,17 +15,34 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity  extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
     private static final String PATH_START = "start";
     private static final String PATH_MESSAGE = "message";
+
+    @BindView(R.id.tvMessage)
+    TextView tvMessage;
+    @BindView(R.id.edtmessage)
+    EditText edtmessage;
+    @BindView(R.id.btnmessage)
+    Button btnmessage;
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
         final TextView tvMessage = findViewById(R.id.tvMessage);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference reference = database.getReference(PATH_START).child(PATH_MESSAGE);
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference(PATH_START).child(PATH_MESSAGE);
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -35,5 +54,10 @@ public class MainActivity  extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Error al consultar en Firebase", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    @OnClick(R.id.btnmessage)
+    public void onViewClicked() {
+        reference.setValue(edtmessage.getText().toString().trim());
     }
 }
